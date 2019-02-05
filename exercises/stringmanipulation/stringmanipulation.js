d4763e5c6afba8dd002772d117fe3dc8b32cedf6
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-04 17:41:35 -0700

    push on mario project

--- exercises/stringmanipulation/stringmanipulation.js
+++ exercises/stringmanipulation/stringmanipulation.js
@@ -1,19 +1,23 @@
 var readline = require('readline-sync');
 
 
-// var yell = readline.question('Tell me what to yell! ')
-// console.log(yell.toUpperCase());
+var yell = readline.question('Tell me what to yell! ')
+console.log(yell.toUpperCase());
 
-// var length = readline.question('What do you want the length of? ')
-// console.log(length.length);
+var length = readline.question('What do you want the length of? ')
+console.log(length.length);
 
-// var concat1 = readline.question('Lets put two strings together! First string? ')
-// var concat2 = readline.question('2nd string? ')
-// console.log(concat1 + concat2);
+var concat1 = readline.question('Lets put two strings together! First string? ')
+var concat2 = readline.question('2nd string? ')
+console.log(concat1 + concat2);
 
-var longmessage = readline.question('Tell me a story! ');
-// var newmessage = Math.floor(longmessage.length/2)
-// console.log(longmessage.slice(newmessage, longmessage.length));
+var longmessage = readline.question('Tell me a story... ');
+if (longmessage.length >= 20) {
+var newmessage = Math.floor(longmessage.length/2)
+console.log(longmessage.slice(newmessage, longmessage.length));
+} else {
+    console.log(longmessage);
+}
 
 var newcut = readline.question('Where does it hurt? (type a number) ');
-console.log(longmessage.slice((newcut + 1), longmessage.length));
+console.log(longmessage.slice(newcut, longmessage.length));
---
+++ projects/mariopestcontrol/mariopestcontrol.js
@@ -0,0 +1,9 @@
+document.getElementById('button').addEventListener('click', submit);
+function submit() {
+    var goomba = document.getElementById('goombaform').value * 5;
+    var bobomb = document.getElementById('bobombform').value * 7;
+    var cheep = document.getElementById('cheepform').value * 11;
+    sum = +goomba + +bobomb + +cheep;
+    console.log(sum);
+    document.getElementById('output').textContent = `You won ${sum} coins!`
+}
