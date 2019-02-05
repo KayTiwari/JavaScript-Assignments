4f57b9c2d4f627b6d6cf355ddf0b27bbca589491
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-04 18:21:53 -0700

    changes

--- projects/mariopestcontrol/mariopestcontrol.js
+++ projects/mariopestcontrol/mariopestcontrol.js
@@ -5,5 +5,9 @@ function submit() {
     var cheep = document.getElementById('cheepform').value * 11;
     sum = +goomba + +bobomb + +cheep;
     console.log(sum);
+    if (sum === 0) {
+        document.getElementById('output').textContent = `Get to work!`
+    } else {
     document.getElementById('output').textContent = `You won ${sum} coins!`
+}
 }
