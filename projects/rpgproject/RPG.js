c088f66e2d2bdcbcf9ae1ef8206cdb9cab7c7356
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-10 13:08:23 -0700

    push on rpg

--- projects/rpgproject/RPG.js
+++ projects/rpgproject/RPG.js
@@ -1,63 +1,74 @@
 var readline = require('readline-sync');
 var isAlive = true;
 var hasWon = false;
-function Abe(weaponName='JUSTICE') {
-    this.hp = 100;
-    this.mattack = Math.floor(Math.random()*20)+50;
-    this.shot = 666;
-    this.weapons = [weaponName.toUppercase(), ];
-    this.items = [];
+
+var Abe = {
+    hp: 100,
+    mattack: 50,
+    shot: 666,
+    axe: 'Honesty',
+    items: []
 }
-function Chernobog() {
-    this.name = 'Chernobog';
-    this.hp = 70;
-    this.attack = Math.floor(Math.random()*5)+5;
-    this.def = 0;
+var Chernobog = {
+    name: 'Chernobog',
+    hp: 70,
+    attack: Math.floor(Math.random()*5)+5,
+    def: 0,
 }
 function firstfight() {
-    console.log('It\'s Vampire Chernobog');
-    setTimeout(function(){console.log("\'They told me I'd find you here");}, 2000);
+    console.log('It\'s Vampire Chernobog!');
+    console.log("\"They told me I'd find you here\"");
+    ffirstfight();
+    function ffirstfight() {
     var faction = readline.keyIn('What will you do? [M] Melee, [S] Shoot, [I] Inventory, [R] Run ', {limit: 'msir'})
     if (faction = 'm') {
-        console.log(`You swing ${Abe.weaponName} with all your force`)
-        Chernobog.hp -= Abe.mattack
+        console.log(`You swing ${Abe.axe} with all your force`)
+        var battlehit = Abe.mattack + Math.floor(Math.random()*30);
+        console.log(battlehit)
+        Chernobog.hp -= battlehit;
         Abe.hp -= Chernobog.attack;
-        console.log(`Hit for ${Abe.mattack} and took ${Chernobog.attack}`)
-        setTimeout(function(){console.log(`Hit for ${Abe.mattack} and took ${Chernobog.attack}`);}, 2000);
-        setTimeout(function(){console.log(`Chernobog's health is at ${Chernobog.hp}, You're at ${Abe.hp} `);}, 4000);
-    } if (faction = 's') {
+        console.log(`Hit for ${Abe.mattack} and took ${Chernobog.attack}`)`     `   
+        setTimeout(function(){console.log(`Hit for ${battlehit} and took ${Chernobog.attack}`);}, 0000);
+        setTimeout(function(){console.log(`Chernobog's health is at ${Chernobog.hp}, You're at ${Abe.hp} `);}, 0000);
+        ffirstfight();
+    } else if (faction = 's') {
         console.log('You search your pockets for the LIBERTY')
         setTimeout(function(){console.log('...but don\'t find it');}, 2000);
         Abe.hp -= Chernobog.attack;
-    } if (faction = 'i') {
+        ffirstfight();
+    } else if (faction = 'i') {
         console.log(Abe.weapons + Abe.items);
         console.log('Nothing to use here!')
-    } if (faction = 'r') {
+        ffirstfight();
+    } else if (faction = 'r') {
         console.log('NO RUNNING!')
+        ffirstfight();
     }
     if (Abe.hp = 0) {
         isAlive = false;
+        death();
     }
     if (Chernobog.hp = 0) {
         console.log('Chernobog slain!');
         freetime();
     }
 }
+}
 
 
 
 if (isAlive === true) {
 console.log('You wake up in a dark room.')
-setTimeout(function(){console.log('You struggle to remember who you are and what happened.');}, 3000)
-setTimeout(function(){console.log('You are the great vampire slayer, Abraham Lincoln.');}, 5000)
-setTimeout(function(){console.log('...');}, 7000)
-setTimeout(function(){console.log('CRASH!');}, 9000)
-setTimeout(function(){console.log('Somethings coming');}, 10000)
-setTimeout(function(){console.log('You look down and see your axe');}, 11000)
-var weaponName = setTimeout(function(){readline.question('What is your axe\'s name? ');}, 12000)
-setTimeout(function(){firstfight();}, 13000);
-// freetime();
-}
- if (isAlive === false) {
-    console.log('You\'ve failed your mission, try again?');
-}
+setTimeout(function(){console.log('You struggle to remember who you are and what happened.');}, 0000)
+setTimeout(function(){console.log('You are the great vampire slayer, Abraham Lincoln.');}, 0000)
+setTimeout(function(){console.log('...');}, 0000)
+setTimeout(function(){console.log('CRASH!');}, 0000)
+setTimeout(function(){console.log('Somethings coming');}, 0000)
+setTimeout(function(){console.log('You look down and see your axe');}, 000)
+setTimeout(function(){Abe.axe = readline.question('What is your axe\'s name? ');}, 000)}
+setTimeout(function(){firstfight();}, 6000);
+// freetime()
+
+// function death() {
+//     console.log('You\'ve failed your mission, try again?');
+// }
