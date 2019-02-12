360b9228ffbdfd63de31d567d7cf942c5d793692
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-12 13:03:04 -0700

    push for today

--- exercises/axiostodo/app.js
+++ exercises/axiostodo/app.js
@@ -1,17 +1,26 @@
 var xhr = new XMLHttpRequest();
 
-xhr.open("GET", "https://api.vschool.io/Abhi/todo", true);
+// xhr.onreadystatechange = function () {
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         var jsonData = xhr.responseText;
+//         var data = JSON.parse(jsonData);
+//         var name = data.name;
+//         document.getElementById("demo1").innerHTML = "JSON results: " + data;
+//         document.getElementById("demo2").innerHTML = "Parsed JSON's 'name' property: " + data.name;
+//     }
+// };
 
-xhr.send();
+// xhr.open("GET", "https://api.vschool.io/Abhi/todo/5c61c68d3d809a761cbd9fad", true);
+
+// xhr.send();
 
 xhr.onreadystatechange = function () {
-    if (xhr.readyState == 4 && xhr.status == 200) {
-        var jsonData = xhr.responseText;
-        var data = JSON.parse(jsonData);
-        var name = data.name;
+    if(xhr.readyState === 4 && xhr.status === 200){
+        var data = JSON.parse(xhr.responseText)
         document.getElementById("demo1").innerHTML = "JSON results: " + data;
-        document.getElementById("demo2").innerHTML = "Parsed JSON's 'name' property: " + data.name;
-        spaceshipurl = data.starships[0];
-        console.log(spaceshipurl);
-    }
+        console.log(data)
+    } 
 };
+
+xhr.open("GET", "https://pokeapi.co/api/v2/pokemon/ditto", true);
+xhr.send();
