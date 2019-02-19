d1ea4628574448183d93c08dae2730920ffa88bf
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-19 09:04:16 -0700

    finish es6

--- exercises/es6prac/es6prac.js
+++ exercises/es6prac/es6prac.js
@@ -36,3 +36,31 @@
 // }
 // console.log(printString(fname = 'kay', age = 23))
 
+// const animals = [
+//     {
+//         type: "dog",
+//         name: "theodore"
+//     },
+//     {
+//         type: "cat",
+//         name: "whiskers"
+//     },
+//     {
+//         type: "pig",
+//         name: "piglette"
+//     },
+//     {
+//         type: "dog",
+//         name: "sparky"
+//     }
+//  ];
+// var newammy =  animals.filter(animal => (animal.type === "dog"));
+// console.log(newammy)
+
+let name = 'Kay';
+let location = 'Salt Lake City';
+console.log(`Hi ${name}!
+
+Welcome to ${location}. 
+
+I hope you enjoy your stay. Please ask the president of ${location} if you need anything.`)
