c870de3c92a4df175977a3108770c9151d72c9a6
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-02-22 16:11:14 -0700

    2nd push for today

--- exercises/djreact/src/App.js
+++ exercises/djreact/src/App.js
@@ -12,24 +12,75 @@ class App extends Component {
         super();
         this.state = {
             box1: 'white',
-            box2: 'black',
+            box2: 'white',
+            box3: 'white',
+            box4: 'white',
+            border: 0
+            
             
             
         }
     }
     BW = () => {
-        if (this.state === box1) {
-            this.setState(box1 => box2);
+        if (this.state.box1 === 'white') {
+            let newColor = 'black';
+            this.setState({box1: newColor})
+            this.setState({box2: newColor})
+            this.setState({box3: newColor})
+            this.setState({box4: newColor})
+            }
+         else {
+            let newColor = 'white';
+            this.setState({box1: newColor})
+            this.setState({box2: newColor})
+            this.setState({box3: newColor})
+            this.setState({box4: newColor})
         }
+    }
+    purpz = () => {
+        let newColor = 'purple';
+        this.setState({box1: newColor})
+        this.setState({box2: newColor})
+    }
+    BL = () => {
+        let newColor = 'blue';
+        this.setState({box3: newColor})
+    }
+    UE = () => {
+        let newColor = 'blue';
+        this.setState({box4: newColor})
+    }
+    rad = () => {
+        let newBorder = 50;
+        this.setState({border: newBorder})
+    }
+    i = () => {
+        let newBorder = 70;
+        this.setState({border: newBorder})
+    }
+    u = () => {
+        let newBorder = 20;
+        this.setState({border: newBorder})
+    }
+    s = () => {
+        let newBorder = 1000;
+        this.setState({border: newBorder})
     }
     render() {
         return (
             <div className='main'>
-                <div className='one' style={{backgroundColor: this.state.box1}}></div>
-                <div className='two' style={{backgroundColor: this.state.box1}}></div>
-                <div className='three' style={{backgroundColor: this.state.box1}}></div>
-                <div className='four' style={{backgroundColor: this.state.box1}}></div>
+                <div className='one' style={{backgroundColor: this.state.box1, borderRadius: this.state.border}}></div>
+                <div className='two' style={{backgroundColor: this.state.box2, borderRadius: this.state.border}}></div>
+                <div className='three' style={{backgroundColor: this.state.box3, borderRadius: this.state.border}}></div>
+                <div className='four' style={{backgroundColor: this.state.box4, borderRadius: this.state.border}}></div>
                 <button onClick={this.BW}>B/W</button>
+                <button onClick={this.purpz}>Purpz</button>
+                <button onClick={this.BL}>BL</button>
+                <button onClick={this.UE}>UE</button>
+                <button onClick={this.rad}>rad</button>
+                <button onClick={this.i}>i</button>
+                <button onClick={this.u}>u</button>
+                <button onClick={this.s}>s</button>
             </div>
         )
     }
