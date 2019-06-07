7e2056665ef8fe264ac41d66f77baca831fa8852
Abhinav Tiwari <abhi.kay.tiwari@gmail.com>
2019-06-06 22:15:59 -0600

    final commit before deleting for macbook

--- exercises/Func practice/app.js
+++
@@ -1,6 +0,0 @@
-
-var printName = function (name, age) {
-    console.log("Hello class my name is " + name + " and my age is " + age, "todays date is" ,Date().toString());
-};
-
-printName("Abhi Tiwari", 23);
